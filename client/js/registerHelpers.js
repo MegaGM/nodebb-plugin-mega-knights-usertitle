a95c5c5e15f82e082b4a6becd3d81fd086ca1f5d
Mega <Aza.Noriega@gmail.com>
2016-03-14 16:53:31 +0500

    Mark some chunks of code as DEPRECATED

--- client/js/registerHelpers.js
+++ client/js/registerHelpers.js
@@ -35,16 +35,19 @@ $(document).on('ready', function (e) {
 				.value();
 		}
 
-	function getPostUserStyle (name) {
-		if (name.match(leader)) return 'group-leader';
-		if (name.match(foreman)) return 'group-foreman';
-		if (name.match(officer)) return 'group-officer';
-		if (name.match(recruiter)) return 'group-recruiter';
-		if (name.match(knight)) return 'group-knight';
-		if (name.match(friend)) return 'group-friend';
-		if (name.match(guest)) return 'group-guest';
-		return 'group-guest';
-	}
+	/**
+	 * DEPRECATED or NOT IN USE YET
+	 */
+	// function getPostUserStyle (name) {
+	// 	if (name.match(leader)) return 'group-leader';
+	// 	if (name.match(foreman)) return 'group-foreman';
+	// 	if (name.match(officer)) return 'group-officer';
+	// 	if (name.match(recruiter)) return 'group-recruiter';
+	// 	if (name.match(knight)) return 'group-knight';
+	// 	if (name.match(friend)) return 'group-friend';
+	// 	if (name.match(guest)) return 'group-guest';
+	// 	return 'group-guest';
+	// }
 
 		function wrapUserTitle (group) {
 			if (void 0 == group) return '';
@@ -80,12 +83,16 @@ $(document).on('ready', function (e) {
 		/**
 		 * Register helpers
 		 */
-		templates.registerHelper('pickUserPostStyle', function (user) {
-			console.log('client side pickUserPostStyle', user);
-			if (user && _.isArray(user)) user = {groups: user};
-			if (!user || _.isEmpty(user.groups)) return 'group-guest';
-			return user.userPostStyle = getPostUserStyle(_.first(sortGroups(user.groups)).name);
-		});
+
+		/**
+		 * DEPRECATED or NOT IN USE YET
+		 */
+		// templates.registerHelper('pickUserPostStyle', function (user) {
+		// 	console.log('client side pickUserPostStyle', user);
+		// 	if (user && _.isArray(user)) user = {groups: user};
+		// 	if (!user || _.isEmpty(user.groups)) return 'group-guest';
+		// 	return user.userPostStyle = getPostUserStyle(_.first(sortGroups(user.groups)).name);
+		// });
 
 		templates.registerHelper('pickUserGroups', function (groups) {
 			if (_.isEmpty(groups)) return guest;
--- library.js
+++ library.js
@@ -18,24 +18,25 @@
 
 
 	/**
+	 * DEPRECATED or NOT IN USE YET
 	 * Change groups' titles to delete *APB* or *GTA* from them
 	 * on account.tpl
 	 */
-	Plugin.changeName = function (data, callback) {
-		if (data.userData.groups && !_.isEmpty(data.userData.groups)) {
-			data.userData.groups.forEach(function (group) {
-				group.userTitle = group.name
-					.replace(/Лидеры/i, 'Лидер')
-					.replace(/Главы/i, 'Глава')
-					.replace(/Офицеры/i, 'Офицер')
-					.replace(/Рекрутеры/i, 'Рекрутер')
-					.replace(/Рыцари/i, 'Рыцарь')
-					.replace(/Соратники/i, 'Соратник');
-			});
-		}
-
-		callback(null, data);
-	};
+	// Plugin.changeName = function (data, callback) {
+	// 	if (data.userData.groups && !_.isEmpty(data.userData.groups)) {
+	// 		data.userData.groups.forEach(function (group) {
+	// 			group.userTitle = group.name
+	// 				.replace(/Лидеры/i, 'Лидер')
+	// 				.replace(/Главы/i, 'Глава')
+	// 				.replace(/Офицеры/i, 'Офицер')
+	// 				.replace(/Рекрутеры/i, 'Рекрутер')
+	// 				.replace(/Рыцари/i, 'Рыцарь')
+	// 				.replace(/Соратники/i, 'Соратник');
+	// 		});
+	// 	}
+	//
+	// 	callback(null, data);
+	// };
 
 
 	/**
@@ -73,16 +74,19 @@
 			.value();
 	}
 
-	function getPostUserStyle (name) {
-		if (name.match(leader)) return 'group-leader';
-		if (name.match(foreman)) return 'group-foreman';
-		if (name.match(officer)) return 'group-officer';
-		if (name.match(recruiter)) return 'group-recruiter';
-		if (name.match(knight)) return 'group-knight';
-		if (name.match(friend)) return 'group-friend';
-		if (name.match(guest)) return 'group-guest';
-		return 'group-guest';
-	}
+	/**
+	 * DEPRECATED or NOT IN USE YET
+	 */
+	// function getPostUserStyle (name) {
+	// 	if (name.match(leader)) return 'group-leader';
+	// 	if (name.match(foreman)) return 'group-foreman';
+	// 	if (name.match(officer)) return 'group-officer';
+	// 	if (name.match(recruiter)) return 'group-recruiter';
+	// 	if (name.match(knight)) return 'group-knight';
+	// 	if (name.match(friend)) return 'group-friend';
+	// 	if (name.match(guest)) return 'group-guest';
+	// 	return 'group-guest';
+	// }
 
 	function wrapUserTitle (group) {
 		if (void 0 == group) return '';
@@ -129,11 +133,15 @@
 		/**
 		 * Register helpers
 		 */
-		templates.registerHelper('pickUserPostStyle', function (user) {
-			if (user && _.isArray(user)) user = {groups: user};
-			if (!user || _.isEmpty(user.groups)) return 'group-guest';
-			return user.userPostStyle = getPostUserStyle(_.first(sortGroups(user.groups)).name);
-		});
+
+		/**
+		 * DEPRECATED or NOT IN USE YET
+		 */
+		// templates.registerHelper('pickUserPostStyle', function (user) {
+		// 	if (user && _.isArray(user)) user = {groups: user};
+		// 	if (!user || _.isEmpty(user.groups)) return 'group-guest';
+		// 	return user.userPostStyle = getPostUserStyle(_.first(sortGroups(user.groups)).name);
+		// });
 
 		templates.registerHelper('pickUserGroups', function (groups) {
 			if (_.isEmpty(groups)) return guest;
